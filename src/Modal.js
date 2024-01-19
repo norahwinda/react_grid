8f9c005a31e63440451edb399f3f115124dd5c2b
norahwinda <norahwinda48@gmail.com>
2024-01-19 22:12:44 +0300

    grids rendered just in boxes

--- src/Modal.js
+++ src/Modal.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Image from './assets/image.jpg'
+import Image from './assets/image.svg'
 
 export default function Modal() {
   return (
-    <div style={{width:'51cm', height:'51cm', borderRadius: '5px', position: 'absolute', backgroundColor: 'blue'}}>
-      <img src={Image} alt='' style={{width:'51cm', height:'51cm',}}/>
+    <div style={{width:'49cm', height:'49cm', backgroundColor: 'blue', display: "flex", flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', borderRadius: '5px'}}>
+      {Array.from(Array(250000), (_, i)=><div key={i} style={{width:'0.1cm', height:'0.1cm', backgroundColor: 'purple', zIndex: '10', marginLeft: '2px', marginRight: '2px', marginTop: '2px'}}></div>)}
+      <img src={Image} alt='' style={{width: '49cm', height:'49cm', position: 'absolute'}} />
     </div>
   )
 }
--- src/Mymap.js
+++ src/Mymap.js
@@ -4,8 +4,7 @@ import Modal from './Modal'
 function Mymap() {
     
   return (
-    <div style={{width:'52cm', height:'52cm',backgroundColor:'red', display: "flex", flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', borderRadius: '5px'}}>
-        {Array.from(Array(27040), (_, i)=><div key={i} style={{width:'0.1cm', height:'0.1cm', backgroundColor: 'purple', zIndex: '10', marginLeft: '2px', marginRight: '2px', marginTop: '2px'}}></div>)}
+    <div style={{width:'50cm', height:'50cm',backgroundColor:'red', display: "flex", flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', borderRadius: '5px'}}>
       <Modal />
     </div>
   )
